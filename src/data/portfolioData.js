f263b34b897b1358f67850b9235beba06f433770
Piyumal Sandeepa <mkpiyumalsandeepa@gmail.com>
2026-03-18 21:54:59 +0530

    add linkedin

--- src/data/portfolioData.js
+++ src/data/portfolioData.js
@@ -29,9 +29,9 @@ export const personalInfo = {
    profileImage: "/profile.jpg",
     social: {
         github: "https://github.com/PiyumalSandeepa",
-        linkedin: "https://linkedin.com/in/piyumalsandeepa",
+        linkedin: "www.linkedin.com/in/piyumal-sandeepa-9ab976237",
         twitter: "https://twitter.com/",
-        instagram: "https://instagram.com/",
+        instagram: "https://www.instagram.com/piyumal_sandeepa_99?igsh=MXB3OXVpOGpnbnk1Mg==",
     },
 };
 
